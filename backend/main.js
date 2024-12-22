bf0a22f29412a9b9f076dce78ffa2c02dd62430e
dhanushnaruto <153058416+dhanushnaruto@users.noreply.github.com>
2024-12-22 23:18:31 +0530

    Update main.js
    asdasd

--- backend/main.js
+++ backend/main.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({ origin: 'http://localhost:5173'}))
+app.use(cors({ origin: 'https://anantadi-pjt-2.onrender.com/'}))
 
 dotenv.config()
 connectDb()
