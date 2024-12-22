6046fc29bac842eefe93cd6a80a0fcd2e5db2272
dhanushnaruto <153058416+dhanushnaruto@users.noreply.github.com>
2024-12-22 23:48:54 +0530

    Update db.js
    fghfdg

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose"
 
 const connectDb= async() => {
     try{
-        await mongoose.connect(process.env.CONNECTION_KEY, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,})
-        console.log('db is connected')
+        await mongoose.connect(process.env.CONNECTION_KEY)
+        console.log('db is connected)
     }
     catch(e){
         console.error(e.message)
