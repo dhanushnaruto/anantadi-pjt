6d781b1c0347c5ae51aac8765aeb9db916bdb4ef
dhanushnaruto <153058416+dhanushnaruto@users.noreply.github.com>
2024-12-23 08:08:27 +0530

    Update main.js
    sfds

--- backend/main.js
+++ backend/main.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({ origin: 'https://anantadi-pjt-2.onrender.com/'}))
+app.use(cors({ origin: 'https://anantadi-pjt-2.onrender.com'}))
 
 dotenv.config()
 connectDb()
